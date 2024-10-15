5150dfa83b51d00fdc313456486495c39194c1ae
Sam <samuel.sum@stu.ocsb.ca>
2024-10-15 11:10:41 -0400

    did most of the tasks

--- main.js
+++ main.js
@@ -11,3 +11,78 @@
 
 'use strict';
 
+
+// TASK PART 1
+
+function to_fahrenheit(celsius) {
+    
+    let fahrenheit = Math.round((celsius * 9/5) + 32); // Fahrenheit = (Celsius * 9/5) + 32
+
+    return fahrenheit;
+
+}
+
+
+// TASK PART 2
+
+function to_celsius(fahrenheit) {
+    
+    let celsius = Math.round ((fahrenheit - 32) * 5/9); // Celsius = (Fahrenheit - 32) * 5/9
+
+    return celsius;
+
+}
+
+
+// TASK PART 3 - SKIP
+
+function roll_dice(qty, sides) {
+
+    let sum = randInt(1, sides)
+
+    return sum;
+
+}
+
+function randInt(min, max) {
+    
+    let rnd = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return rnd;
+
+}
+
+
+//TASK PART 4
+
+function area_circle(radius) {
+
+    let area = Math.PI * (radius**2);
+
+    return area;
+
+}
+
+
+// TASK PART 5
+
+function cylinder_volume(radius, height) {
+
+    let volume = area_circle(radius) * height;
+
+    return volume;
+
+}
+
+
+// TASK PART 6
+
+function percent(score, total) {
+
+    let percent_grade = (score / total) * 100
+
+    return (`${percent_grade}%`)
+
+}
+
+
